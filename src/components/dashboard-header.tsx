eeda830f79fb0e3ba6070f1023f4993abf606f0b
Juan Jdvf <waristiki17@gmail.com>
2025-08-05 19:28:17 +0000

    QUITA EL BOTON DE BUSQUEDA

--- src/components/dashboard-header.tsx
+++ src/components/dashboard-header.tsx
@@ -25,18 +25,6 @@ export default function DashboardHeader() {
     <header className="flex h-14 items-center gap-4 border-b bg-background/95 backdrop-blur-sm px-4 lg:h-[60px] lg:px-6 sticky top-0 z-30">
       <div className="flex items-center gap-2">
         <SidebarTrigger className="md:hidden" />
-        <div className="hidden md:block w-full flex-1">
-          <form>
-            <div className="relative">
-              <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-              <Input
-                type="search"
-                placeholder="Buscar..."
-                className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
-              />
-            </div>
-          </form>
-        </div>
       </div>
       
       <div className="flex items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
