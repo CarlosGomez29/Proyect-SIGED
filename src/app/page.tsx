70bc6f5270ce4c6c9af460031caa077ff2a7a4c3
Carlos Miguel Gomez Perez <waristiki17@gmail.com>
2025-11-06 12:40:34 +0000

    encima de esto, es bueno colocar por lo menos el nombre de las instituci

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,7 +94,10 @@ export default function ProfileSelectionPage() {
         transition={{ duration: 0.5 }}
       >
         <Icons.logo className="h-20 w-20 text-primary mx-auto" />
-        <h1 className="text-4xl font-bold font-headline mt-6">SIGED - DIGEV</h1>
+        <p className="text-muted-foreground mt-4 text-md font-medium">
+          Direccion General de las Escuelas Vocacionales de las Fuerzas Armadas y de la Policia Nacional
+        </p>
+        <h1 className="text-4xl font-bold font-headline mt-2">SIGED - DIGEV</h1>
         <p className="text-muted-foreground mt-2 text-lg max-w-2xl">
          Sistema Integral de Gestion de Estudiantes y Docentes
         </p>
