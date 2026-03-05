cb69d9d4e980c8cd86cde1032587bef7d9478101
Carlos Miguel Gomez Perez <waristiki17@gmail.com>
2026-03-05 23:49:25 +0000

    Try fixing this error: `Build Error: Export Button doesn't exist in targ

--- src/app/dashboard-admin/ajustes/page.tsx
+++ src/app/dashboard-admin/ajustes/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from 'react';
-import { Button } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
